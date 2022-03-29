ed88548d6d569071c75ef1e905f071c0d14eeeed
Nick V <nickv@Nicks-MBP.lan>
2022-03-28 21:47:43 -0500

    feat: routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
-import RegistrationPage from './pages/RegistrationPage.js'
+import HomePage from './pages/HomePage.js';
+import RegistrationPage from './pages/RegistrationPage';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <RegistrationPage/>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<HomePage/>}/>
+          <Route path="/register" element={<RegistrationPage/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
