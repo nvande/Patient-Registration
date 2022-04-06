7e7938d40ed60f5aaafd285747e21ea1fc9cadaf
Nick V <nickv@Nicks-MBP.lan>
2022-04-06 17:16:19 -0500

    fix: final polish

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -8,7 +8,7 @@ function HeaderComponent(props){
 	const { isAuthenticated, loading, loginWithRedirect, logout, user } = useAuth0();
 
 	return (
-		<Container className={'mt-3 mt-sm-5 px-md-3 px-lg-5 pt-3 pb-2 pb-md-3 text-muted text-center text-md-start pageHeader bg-info text-white clearfix'}>
+		<Container className={'mt-3 mt-sm-5 px-md-3 px-lg-5 pt-3 pb-2 pb-md-2 text-muted text-center text-md-start pageHeader bg-info text-white clearfix'}>
 			<Row>
 				<Col sm="12" md="6">
 					<Link to='/'>
@@ -18,9 +18,9 @@ function HeaderComponent(props){
 				{ isAuthenticated && !loading &&
 					<Col sm="12" md="6">
 						<div className="d-inline">
-							<Button className={"float-end mt-2 mt-md-1 mb-1"} onClick={()=>logout({ returnTo: `http://localhost:3000` })}>Log Out</Button>
+							<Button className={"float-end mt-2 mt-md-0 mb-1"} onClick={()=>logout({ returnTo: `http://localhost:3000` })}>Log Out</Button>
 							<img src={user.picture} className="float-start float-md-end rounded-circle img-thumbnail mt-0 mt-md-0 me-2 me-md-4 userPhoto"/>
-							<span className="float-start float-md-end mt-2 my-md-2 me-2 h4 h-100 align-middle text-white">{user.nickname}</span>
+							<span className="float-start float-md-end mt-1 me-2 h4 h-100 align-middle text-white">{user.nickname}</span>
 						</div>
 					</Col>
 				}
